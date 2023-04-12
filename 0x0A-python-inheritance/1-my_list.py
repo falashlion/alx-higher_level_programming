c48753976dc5bd26181e4972798a7140c11f773b
fochu felix awah <forchufelix@gmail.com>
2023-04-12 05:00:22 -0700

    gti th

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-""" The MyList class inherits from list """
+"""
+contains the MyList class
+"""
 
 
 class MyList(list):
-    """ Inherits from list """
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
-        """ prints the list, but sorted in an ascending order """
+        """prints the sorted list"""
         print(sorted(self))
