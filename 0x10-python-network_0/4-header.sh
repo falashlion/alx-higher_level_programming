cf4c45d972de3e894e736abe92084565aad607bf
fochu felix awah <forchufelix@gmail.com>
2023-07-03 13:21:32 -0700

    final commit

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl "$1" -sX GET -H "X-School-User-Id: 98"
